c1de9b4150a303f5963ccbca58ec57be1a96e238
Adam Sturrock <adam@molt.in>
2015-03-06 23:56:36 -0800

    Update Entry.php

--- src/Moltin/SDK/Facade/Entry.php
+++ src/Moltin/SDK/Facade/Entry.php
@@ -36,31 +36,31 @@ class Entry
 
 	public static function Find($flow, $terms = array())
 	{
-		return self::$sdk->get('flows/'.$flow.'/entries', $terms);
+		return self::$sdk->get('flow/'.$flow.'/entries', $terms);
 	}
 
 	public static function Listing($flow = null, $terms = array())
 	{
-		return self::$sdk->get('flows/'.$flow.'/entries', $terms);
+		return self::$sdk->get('flow/'.$flow.'/entries', $terms);
 	}	
 
 	public static function Create($flow, $data)
 	{
-		return self::$sdk->post('flows/'.$flow.'/entry', $data);
+		return self::$sdk->post('flow/'.$flow.'/entry', $data);
 	}
 
 	public static function Update($flow, $id, $data)
 	{
-		return self::$sdk->put('flows/'.$flow.'/entry/'.$id, $data);
+		return self::$sdk->put('flow/'.$flow.'/entry/'.$id, $data);
 	}
 
 	public static function Fields($flow = null, $id)
 	{
-		return self::$sdk->fields('flows/'.$flow.'/entry', $id);
+		return self::$sdk->fields('flow/'.$flow.'/entry', $id);
 	}
 
 	public static function Delete($flow, $id)
 	{
-		return self::$sdk->delete('flows/'.$flow.'/entry/'.$id);
+		return self::$sdk->delete('flow/'.$flow.'/entry/'.$id);
 	}
 }
